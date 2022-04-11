67e042267baa18ea01e044a9b5c3cbfa44ed46f5
unknown <tomasfw94@gmail.com>
2022-04-11 03:23:04 -0300

    Syntax fixes

--- src/com/spirent/Main.java
+++ src/com/spirent/Main.java
@@ -12,6 +12,10 @@ import java.util.Set;
 
 public class Main {
 
+    public static final int FULL_PHONE_NUMBER_LENGTH = 11;
+    public static final int PHONE_NUMBER_LENGTH_WITHOUT_COUNTRYCODE = 10;
+    public static final int PHONE_NUMBER_LENGTH_WITHOUT_COUNTRYCODE_AND_CITYCODE = 7;
+
     public static void main(String[] args) throws IOException {
         File directory = new File("C:/path/to/root/directory");
         Set<PhoneNumber> phoneNumbers = readPhoneNumbersFromDirectoryFiles(directory.listFiles());
@@ -28,24 +32,24 @@ public class Main {
 
             if (file.isFile() && file.getName().endsWith(".txt")) {
                 String fileContent = FileUtils.readFileToString(file, Charset.defaultCharset());
-                String formattedFileContent = fileContent.replaceAll("\\s+","").replaceAll("\\+", "").replaceAll("-", "").replaceAll("\\(", "").replaceAll("\\)", "");
-                phoneNumbers.addAll(readPhoneNumbersFromFormattedFileContent(formattedFileContent));
+                String normalizedFileContent = fileContent.replaceAll("\\s+","").replaceAll("\\+", "").replaceAll("-", "").replaceAll("\\(", "").replaceAll("\\)", "");
+                phoneNumbers.addAll(readPhoneNumbersFromNormalizedFileContent(normalizedFileContent));
             }
         }
         return phoneNumbers;
     }
 
-    private static Set<PhoneNumber> readPhoneNumbersFromFormattedFileContent(String formattedFileContent) {
+    private static Set<PhoneNumber> readPhoneNumbersFromNormalizedFileContent(String normalizedFileContent) {
         Set<PhoneNumber> readPhoneNumbers = new HashSet<>();
-        String[] rawNumbers = formattedFileContent.split(",");
+        String[] rawNumbers = normalizedFileContent.split(",");
 
         PhoneNumber phoneNumber = null;
         for (String rawNumber: rawNumbers) {
-            if(rawNumber.length() == 7){
+            if(rawNumber.length() == PHONE_NUMBER_LENGTH_WITHOUT_COUNTRYCODE_AND_CITYCODE){
                 phoneNumber = new PhoneNumber(rawNumber);
-            } else if(rawNumber.length() == 10){
+            } else if(rawNumber.length() == PHONE_NUMBER_LENGTH_WITHOUT_COUNTRYCODE){
                 phoneNumber = new PhoneNumber(rawNumber.substring(0, 3), rawNumber.substring(3));
-            } else if(rawNumber.length() == 11){
+            } else if(rawNumber.length() == FULL_PHONE_NUMBER_LENGTH){
                 phoneNumber = new PhoneNumber(rawNumber.substring(0, 1), rawNumber.substring(1, 4), rawNumber.substring(4));
             }
             if(phoneNumber != null){
